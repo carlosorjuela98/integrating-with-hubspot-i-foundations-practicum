57c35d14f388c3959c0fe703e01fa075d49a5c38
Carlos Orjuela <orjuelacarlos80@gmail.com>
2025-08-18 16:35:47 -0500

    final fixes and readme link added

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ app.get('/update-cobj', (req, res) => {
     res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
 
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+
+// * Code for Route 3 goes here
 
 app.post('/update-cobj', async (req, res) => {
     const { name, ram, price__usd_ } = req.body;
@@ -73,11 +76,6 @@ app.post('/update-cobj', async (req, res) => {
     }
 });
 
-
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
